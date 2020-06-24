16674b2b5ed8ba3716e16f803a16100c2e275836
Mateusz <Mateusz@25.68.226.201>
2020-06-24 21:38:27 +0200

    Hello

---
+++ questionaire-jsp-servlet-jbcd/src/net/java/questionaire/model/Result.java
@@ -0,0 +1,25 @@
+package net.java.questionaire.model;
+
+public class Result {
+private String name;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSurname() {
+	return surname;
+}
+public void setSurname(String surname) {
+	this.surname = surname;
+}
+public String getOpinion() {
+	return opinion;
+}
+public void setOpinion(String opinion) {
+	this.opinion = opinion;
+}
+private String surname;
+private String opinion;
+}
---
+++ questionaire-jsp-servlet-jbcd/src/snippet/Snippet.java
@@ -0,0 +1,15 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		package net.javaguides.jsp.jdbc.database;
+		
+		import java.sql.Connection;
+		import java.sql.DriverManager;
+		import java.sql.PreparedStatement;
+		import java.sql.SQLException;
+		
+		import net.javaguides.jsp.jdbc.bean.Employee;
+	}
+}
+
